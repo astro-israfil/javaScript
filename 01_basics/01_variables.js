881602f789c6ad33e8c53307244f7acdded817a8
astro-israfil <astro.israfil.fi@gmail.com>
2024-04-07 21:06:23 +0600

    learn variable in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 4124;
+let accountName = "Israfil";
+var accountEmail = "israfil@abc.com";
+accountCity = "Dhaka";
+
+
+// accountId = 1234; // Assignment to a constant variable is not allowed
+accountName = "IM";
+accountEmail = "im@abc.com";
+accountCity = "Manikganj";
+
+console.log(accountId);
+
+/* Never to use var keyword to declear a variable */ 
+
+console.table([accountId, accountName, accountEmail, accountCity]);
