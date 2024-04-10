9be52b73849da748a9a9d840d1b2078fcf49a236
astro-israfil <astro.israfil.fi@gmail.com>
2024-04-10 11:51:00 +0600

    data types in javaScript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all code as a newer version of javaScript
+
+// alert("Hello"); // this function is defined on browser not on node js
+
+
+let name = "Israfil"; // string
+let age = 18; // number
+let isLoggedIn = true; // boolean
+// let state = null;
+let state;
+
+
+// Data types in javaScript
+
+// 01. number
+// 02. bigInt
+// 03. string
+// 04. boolean
+// 05. null
+// 06. undefined
+// 07. symbol
+
+// 08. object
+
+console.log(typeof undefined);
+console.log(typeof null);
