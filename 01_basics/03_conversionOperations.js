68003fb850b3c313f3c8722fbc9a963e4e202dbb
astro-israfil <astro.israfil.fi@gmail.com>
2024-04-10 12:27:10 +0600

    Coversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,46 @@
+let score = "";
+
+// console.log(typeof score);
+
+let scoreInNumber = Number(score);
+
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
+
+// Converted to number type
+
+// "Israfil" => NaN
+// "" => 0
+// "33" => 33
+// true => 1; false => 0
+// null => 0;
+// undefined => NaN
+
+
+// Converted to boolean type
+let isLoggedIn = 1;
+
+// console.log(typeof isLoggedIn);
+
+let isLoggedInBoolean = Boolean(isLoggedIn);
+
+// console.log(typeof isLoggedInBoolean);
+// console.log(isLoggedInBoolean);
+
+// Note::
+    // 1 => true; 0 => false
+    // "" => false; "Israfil" => true
+    // null => false
+    // undefined => false
+    // NaN => false
+
+
+
+// Converted to string
+let age = 18;
+console.log(typeof age);
+
+let ageInString = String(age);
+
+console.log(typeof ageInString);
+console.log(ageInString);
