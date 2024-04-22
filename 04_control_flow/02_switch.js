af50ad0de691493651048605f1d37ade8ff81305
astro-israfil <astro.israfil.fi@gmail.com>
2024-04-22 11:04:46 +0600

    learn switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,36 @@
+// switch (key) {
+//     case value:
+//             // statement one
+//         break;
+//     case value:
+//             // statement two
+//         break;
+
+//     default:
+//             // statement
+//         break;
+// }
+
+
+let day = 4;
+
+switch (day) {
+    case 1:
+        console.log("SATURDAY learn to code");
+        break;
+    case 2:
+        console.log("SUNDAY make some project");
+        break;
+    case 3:
+        console.log("MONDAY learn electronic");
+        break;
+    case 4:
+        console.log("Tuesday make some project")
+        break;
+    case 5:
+    case 6:
+    case 7: console.log("Week day");
+        break;
+    default:
+        console.log("Invalid day");
+}
