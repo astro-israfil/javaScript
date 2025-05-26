a82f5150c4cc89382252988a7c5a673ae566b35d
astro-israfil <astro.israfil.fi@gmail.com>
2025-05-26 18:04:16 +0600

    comparison operators in js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+// console.log(undefined > 0) // false
+// console.log(undefined == 0) // false
+// console.log(undefined >= 0) // false
+
+
+console.log("2" == 2)
+console.log("2" === 2)
