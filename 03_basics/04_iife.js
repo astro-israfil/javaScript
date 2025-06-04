3a7a3c713effa2e8e6c8eb8bd82d7b766526111e
astro-israfil <astro.israfil.fi@gmail.com>
2025-06-04 08:48:46 +0600

    Immidiately Invoked Function Expression in JS

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+// Immidiately Invoked Function Expression
+
+
+// Named IIFE
+(function myIIFE() {
+    console.log("DB CONNECTED")
+})();
+
+(() => {
+    console.log("DB CONNECTED SUCCESSFULLY");
+})();
